3f00854a7f73e4f760843a08af6fbcd8e3fce71b
Jason Geiger <448523+jasonjgeiger@users.noreply.github.com>
2023-10-14 10:17:28 -0700

    more form testing

--- src/templates/contact/contact.js
+++ src/templates/contact/contact.js
@@ -14,13 +14,23 @@ const Section = styled.section`
 const Contact = () => {
 
     const handleSubmit = (event) => {
-        alert('Form was submitted');
         event.preventDefault();
+        
+        const myForm = event.target;
+        const formData = new FormData(myForm);
+        
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        })
+        .then(() => console.log("Form successfully submitted"))
+        .catch((error) => alert(error));
     }
 
     return(
         <Section>
-            <form name="contact" method="POST" netlify="">
+            <form onSubmit={handleSubmit} name="contact" method="POST" netlify="">
                 
                 <label>
                     First Name:
